bc4f84c596e44b353c8e062da0c9cb1aea5a93eb
praivesi <praivesi@gmail.com>
2024-08-01 06:21:02 +0900

    feat: make 'apiapp' as  public

--- my_first_aspire/my_first_aspire.AppHost/Program.cs
+++ my_first_aspire/my_first_aspire.AppHost/Program.cs
@@ -5,6 +5,7 @@ var config = builder.Configuration;
 var cache = builder.AddRedis("cache");
 
 var apiapp = builder.AddProject<Projects.MyFirstAzure_ApiApp>("apiapp")
+.WithExternalHttpEndpoints()
 .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
 .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
 .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
